aa2f184139a197b7ec0a6fb07668ec4c3dc3d653
Ramphy Tirana Brito <ramphytb@gmail.com>
2023-10-10 21:59:09 -0400

    proyecto de video 3ra parte

---
+++ 005.video.js
@@ -0,0 +1,20 @@
+
+alert("1 CLICK START BACKGROUND MUSIC")
+alert("2 CLICKS PAUSE BACKGROUND MUSIC")
+/*
+alert("que ")
+alert("eres creacion perfecta")
+*/
+
+function playAudio() {
+    var audio = document.getElementById("myAudio1");
+    audio.play();
+}
+
+function pauseAudio() {
+    var audio = document.getElementById("myAudio1");
+    audio.pause();
+}
+
+//kjhfeoonbovnpoierpgpvvpoihphgihr vp´fui9h qeriúh 
+s
