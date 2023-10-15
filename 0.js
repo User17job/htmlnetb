ff4125c22388d65a2296ca2f9336f8af8d5c4b82
Ramphy Tirana Brito <ramphytb@gmail.com>
2023-10-15 12:36:06 -0400

    cambie los nombres de los arcchivos para ser mas especifico y le hice unos arreglos al archivo 000.1ramaque.html

---
+++ 0.js
@@ -0,0 +1,14 @@
+
+/*
+const audio = new Audio('derramo.mp3');
+audio.play(); audio.pause()*/
+
+/*
+const button = document.createElement('button');
+button.type = 'button';
+button.innerText= 'Play';
+document.body.appendChild(button) */
+
+function ola(){
+    document.write('hola')
+}
